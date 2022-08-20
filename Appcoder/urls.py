2de32ea167f178d7348a77c6f86992b45b284453
Ricardo Da Silva <dasilvaricardog@gmail.com>
2022-08-20 14:00:22 -0400

    Agregando bootstrap a las vistas

--- Appcoder/urls.py
+++ Appcoder/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('curso/', curso),
-    path('profesores/', profesores),
-    path('estudiante/', estudiantes),
-    path('entregable/', entregables),
-    path('cursos/', cursos),
-    path('', inicio), #Se coloca vacio para que sea la paginia de inicio y no necesita ruta
+    path('curso/', curso, name ="curso"),
+    path('profesores/', profesores, name ="profesores"),
+    path('estudiante/', estudiantes, name ="estudiantes"),
+    path('entregable/', entregables, name="entregables"),
+    path('cursos/', cursos, name = "cursos"),
+    path('', inicio, name ="inicio"), #Se coloca vacio para que sea la paginia de inicio y no necesita ruta
 
 ]
